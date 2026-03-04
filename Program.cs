4aa2fe0373bdf4d2b845b5551117841818fa61c6
Petrik Diak <diak@petrik.hu>
2026-03-04 09:03:44 +0100

    aaaaa

--- Program.cs
+++ Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-			Console.WriteLine("1. feladat");
+			// 1. feladat
+			// Konzolos alkalmazás létrehozása Bingo néven
+
+			// 3. feladat
+			// Játékosok beolvasása és eltárolása
+
+			List<BingoJatekos> jatekosok = new List<BingoJatekos>();
+
+			if (File.Exists("nevek.text"))
+			{
+				foreach (var fajl in File.ReadAllLines("nevek.text"))
+				{
+					if (File.Exists(fajl))
+					{
+						jatekosok.Add(new BingoJatekos(fajl));
+					}
+				}
+			}
+			else if (File.Exists("Andi.txt"))
+			{
+				jatekosok.Add(new BingoJatekos("Andi.txt"));
+			}
+
+
 
 		}
 	}
