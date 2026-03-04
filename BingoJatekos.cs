1642b122c91809bb89e593b5422277f53f9d8067
Petrik Diak <diak@petrik.hu>
2026-03-04 08:45:09 +0100

    aa

---
+++ BingoJatekos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+	internal class BingoJatekos
+	{
+		string name;
+		int kartya;
+
+		public BingoJatekos(string name, int kartya)
+		{
+			this.name = name;
+			this.kartya = kartya;
+		}
+
+		public string Name { get => name; set => name = value; }
+		public int Kartya { get => kartya; set => kartya = value; }
+
+		public override string? ToString()
+		{
+			return $"{name} - {kartya}";
+			
+			
+		}
+	}
+
+	
+}
